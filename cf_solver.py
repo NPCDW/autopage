7b883b4071e38767b49b523fb94197e3505a376f
NPCDW <chenyongli0520@gmail.com>
2024-07-23 14:12:34 +0800

    .

--- cf_solver.py
+++ cf_solver.py
@@ -1,8 +1,11 @@
 import random
 import time
+
 from DrissionPage import ChromiumPage
 from DrissionPage import ChromiumOptions
+from DrissionPage.common import Actions
 from config import config
+from pyHM import mouse
 
 
 class LocalSolverCF:
@@ -17,6 +20,7 @@ class LocalSolverCF:
         page = ChromiumPage(co)
         if cookies is not None:
             page.set.cookies(cookies)
+        # page.actions.move(474, 362)
         page.get(url)
         self.page = page
 
@@ -49,12 +53,12 @@ class LocalSolverCF:
     '''
     def capsolver(self):
         shadow_root = self.have_verify()
-        if shadow_root is None:
+        if not shadow_root:
             return None
         while True:
             try:
                 cf_iframe = shadow_root.ele("tag=iframe", timeout=3)
-                if cf_iframe is not None:
+                if cf_iframe:
                     print("加载完成iframe")
                     break
             except:
@@ -64,21 +68,31 @@ class LocalSolverCF:
             try:
                 shadow_root2 = cf_iframe.ele('tag=body', timeout=3).shadow_root
                 button = shadow_root2.ele("tag=input", timeout=3)
-                if button is not None:
+                if button:
                     print("找到按钮")
                     break
             except:
                 print("查找按钮")
                 shadow_root = self.have_verify()
-                if shadow_root is None:
+                if not shadow_root:
                     print("未找到按钮，cf-turnstile-wrapper消失，可能已经过了cf验证")
                     return None
                 time.sleep(1)
         try:
             time.sleep(random.uniform(0.5, 1.5))
             button.click()
+
             # actions = Actions(self.page)
-            # actions.move_to('.cf-turnstile-wrapper', duration=0.5).left(114).up(5).hold().wait(0.01, 0.15).release()  # 104-129  +-13
+            # pre_pos = (actions.curr_x, actions.curr_y)
+            # print("起始坐标：", pre_pos)
+            # wrapper = self.page.ele('.cf-turnstile-wrapper')
+            # print("按钮坐标：", button.rect.screen_location)
+            # print("wrapper坐标：", wrapper.rect.screen_location)
+            # actions.move_to((wrapper.rect.location[0] + 25, wrapper.rect.location[1] + 33))
+            # actions.hold().wait(0.01, 0.15).release()  # 104-129  +-13
+
+            # mouse.move(wrapper.rect.screen_location[0] + 25, wrapper.rect.screen_location[1] + 33)
+            # mouse.click()
         except:
             print(f'click Error')
             time.sleep(1)
