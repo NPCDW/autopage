d42e6341b8f83436d527128a071369a64745a52e
NPCDW <32638459+NPCDW@users.noreply.github.com>
2024-08-17 13:54:42 +0800

    .

--- src/nodeseek_checkin.py
+++ src/nodeseek_checkin.py
@@ -14,6 +14,7 @@ def main():
     cookies = ['session=' + session + '; domain=www.nodeseek.com']
     local_solver_cf = LocalSolverCF('https://www.nodeseek.com/board', cookies)
     local_solver_cf.solver()
+    time.sleep(2)
     try:
         button = local_solver_cf.page.ele("试试手气")
         button.click()
@@ -23,8 +24,10 @@ def main():
     try:
         checkin_result = local_solver_cf.page.ele(".head-info").text
         tg.send_message("NodeSeek: " + checkin_result, message_thread_id=topic_id)
+        print("NodeSeek: " + checkin_result)
     except:
         tg.send_message("NodeSeek: 签到失败", message_thread_id=topic_id)
+        print("NodeSeek: 签到失败")
     # print(local_solver_cf.page.html)
     if config["application"]["close_after_exec"]:
         local_solver_cf.close()
